59b9836cc8ded425f109eb86a2a6008e7e027fa6
Vaibhav Pandey <contact@vaibhavpandey.com>
2024-12-15 03:55:33 +0530

    Build container on Github

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
-import { autocomplete } from '@algolia/autocomplete-js';
-import Alpine from 'alpinejs';
-import collapse from '@alpinejs/collapse';
-import persist from '@alpinejs/persist';
-import { Tooltip } from 'bootstrap';
-import flatpickr from 'flatpickr';
-import $ from 'jquery';
-import select2 from 'select2';
-import 'trumbowyg';
-import './bootstrap';
+import { autocomplete } from "@algolia/autocomplete-js";
+import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
+import persist from "@alpinejs/persist";
+import { Tooltip } from "bootstrap";
+import flatpickr from "flatpickr";
+import $ from "jquery";
+import select2 from "select2";
+import "trumbowyg";
+import "./bootstrap";
 
 Alpine.plugin(collapse);
 Alpine.plugin(persist);
@@ -19,64 +19,71 @@ window.$ = window.jQuery = $;
 
 $.fn.extend({
     datepicker() {
-        return this.each(function() {
+        return this.each(function () {
             flatpickr(this, {
                 allowInput: true,
                 altInput: true,
-                altFormat: 'd/m/Y',
+                altFormat: "d/m/Y",
                 enableTime: false,
-                dateFormat: 'Y-m-d',
+                dateFormat: "Y-m-d",
             });
         });
     },
     datetimepicker() {
-        return this.each(function() {
+        return this.each(function () {
             flatpickr(this, {
                 allowInput: true,
                 altInput: true,
-                altFormat: 'd/m/Y H:i',
+                altFormat: "d/m/Y H:i",
                 enableTime: true,
-                dateFormat: 'Y-m-d H:i:00',
+                dateFormat: "Y-m-d H:i:00",
             });
         });
     },
     dropdown() {
-        return this.each(function() {
+        return this.each(function () {
             $(this).select2({
-                theme: 'bootstrap-5',
-                width: '100%',
+                theme: "bootstrap-5",
+                width: "100%",
             });
         });
     },
     search({ placeholder, source, ...templates }) {
-        return this.each(function() {
+        return this.each(function () {
             const $this = $(this);
             autocomplete({
                 container: this,
                 placeholder: placeholder,
                 getSources({ query: q }) {
-                    return $this.data('indices').map((index) => ({
+                    return $this.data("indices").map((index) => ({
                         getItems() {
-                            return axios.get(source, { params: { index, q } })
+                            return axios
+                                .get(source, { params: { index, q } })
                                 .then(({ data }) => data.data);
                         },
                         getItemUrl({ item }) {
                             return item.url;
                         },
                         onSelect({ item }) {
-                            $this.trigger('change', item);
+                            $this.trigger("change", item);
                         },
-                        sourceId: index + '-source',
+                        sourceId: index + "-source",
                         templates: {
                             header({ html, items }) {
                                 if (items.length) {
                                     return html`
-                                        <p class="small text-muted text-center">Showing ${items.length} ${index}.</p>
+                                        <p class="small text-muted text-center">
+                                            Showing ${items.length} ${index}.
+                                        </p>
                                     `;
                                 }
                             },
                             noResults({ html }) {
-                                return html`<p class="small text-muted text-center mb-0">No ${index} found.</p>`;
+                                return html`<p
+                                    class="small text-muted text-center mb-0"
+                                >
+                                    No ${index} found.
+                                </p>`;
                             },
                             ...templates,
                         },
@@ -86,15 +93,20 @@ $.fn.extend({
         });
     },
     wysiwyg() {
-        return this.each(function() {
+        return this.each(function () {
             $(this).trumbowyg({
                 autogrow: true,
                 btns: [
-                    ['strong', 'em'],
-                    ['unorderedList', 'orderedList'],
-                    ['link'],
-                    ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
-                    ['fullscreen'],
+                    ["strong", "em"],
+                    ["unorderedList", "orderedList"],
+                    ["link"],
+                    [
+                        "justifyLeft",
+                        "justifyCenter",
+                        "justifyRight",
+                        "justifyFull",
+                    ],
+                    ["fullscreen"],
                 ],
                 svgPath: trumbowyg_icons,
             });
@@ -102,35 +114,40 @@ $.fn.extend({
     },
 });
 
-$('.alert:not(.alert-important)')
+$(".alert:not(.alert-important)")
     .delay(5 * 1000)
     .fadeOut(350);
 
-$('body')
-    .on('click', 'form :submit', function () {
+$("body")
+    .on("click", "form :submit", function () {
         const $input = $(this);
-        $input.parents('form').find(':submit').data('clicked', false);
-        $input.data('clicked', true)
+        $input.parents("form").find(":submit").data("clicked", false);
+        $input.data("clicked", true);
     })
-    .on('submit', 'form', function () {
-        const $input = $(':submit', this)
-            .prop('disabled', true)
+    .on("submit", "form", function () {
+        const $input = $(":submit", this)
+            .prop("disabled", true)
             .filter(function () {
-                return $(this).data('clicked') === true
+                return $(this).data("clicked") === true;
             })
             .first();
-        if ($input.attr('name')) {
-            $input.parents('form')
+        if ($input.attr("name")) {
+            $input
+                .parents("form")
                 .append(
                     $('<input type="hidden">')
-                        .attr('name', $input.attr('name'))
-                        .val($input.val())
-                )
+                        .attr("name", $input.attr("name"))
+                        .val($input.val()),
+                );
         }
-        const $icon = $input.find('i');
+        const $icon = $input.find("i");
         if ($icon.length > 0) {
-            const margin = $icon.hasClass('ms-1') ? 'ms-1' : $icon.hasClass('me-1') ? 'me-1' : '';
-            $icon.attr('class', `fa-solid fa-circle-notch fa-spin ${margin}`)
+            const margin = $icon.hasClass("ms-1")
+                ? "ms-1"
+                : $icon.hasClass("me-1")
+                  ? "me-1"
+                  : "";
+            $icon.attr("class", `fa-solid fa-circle-notch fa-spin ${margin}`);
         }
     });
 
@@ -147,7 +164,8 @@ $('[data-widget="dropdown"]').dropdown();
 
 $('[data-widget="wysiwyg"]').wysiwyg();
 
-([].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]')))
+[].slice
+    .call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     .forEach((el) => new Tooltip(el));
 
 Alpine.start();
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import inject from '@rollup/plugin-inject';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
     plugins: [
         inject({
-            jQuery: 'jquery',
-            $: 'jquery',
-            include: [
-                '**/trumbowyg/**/*',
-            ],
+            jQuery: "jquery",
+            $: "jquery",
+            include: ["**/trumbowyg/**/*"],
         }),
         laravel({
             input: [
-                'resources/js/app.js',
-                'resources/sass/app.scss',
-                'resources/styl/flatpickr.styl',
+                "resources/js/app.js",
+                "resources/sass/app.scss",
+                "resources/styl/flatpickr.styl",
             ],
             refresh: true,
         }),
